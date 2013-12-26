2be639bd32f1f0c413664be2ebebb50260bb9e0c
megayu <yuhg2310@gmail.com>
2013-12-26 11:21:50 +0800

    Rename functions & add start/stop functions for testing using the shell

--- src/erlzk.erl
+++ src/erlzk.erl
@@ -2,7 +2,7 @@
 
 -include_lib("../include/erlzk.hrl").
 
--export([start_link/2, start_link/3, start_link/4, stop/1]).
+-export([start/0, stop/0, connect/2, connect/3, connect/4, close/1]).
 -export([create/2, create/3, create/4, create/5, delete/2, delete/3, exists/2, exists/3,
          get_data/2, get_data/3, set_data/3, set_data/4, get_acl/2, set_acl/3, set_acl/4,
          get_children/2, get_children/3, sync/2, get_children2/2, get_children2/3,
@@ -12,19 +12,27 @@
 %% ===================================================================
 %% Initiate Functions
 %% ===================================================================
-start_link(ServerList, Timeout) ->
-    start_link(ServerList, Timeout, []).
+start() ->
+    application:load(erlzk),
+    erlzk_app:ensure_deps_started(),
+    application:start(erlzk).
 
-start_link(ServerList, Timeout, Options) when is_list(Options) ->
+stop() ->
+    application:stop(erlzk).
+
+connect(ServerList, Timeout) ->
+    connect(ServerList, Timeout, []).
+
+connect(ServerList, Timeout, Options) when is_list(Options) ->
     erlzk_conn_sup:start_conn([ServerList, Timeout, Options]);
 
-start_link(ServerName, ServerList, Timeout) when is_integer(Timeout) ->
-    start_link(ServerName, ServerList, Timeout, []).
+connect(ServerName, ServerList, Timeout) when is_integer(Timeout) ->
+    connect(ServerName, ServerList, Timeout, []).
 
-start_link(ServerName, ServerList, Timeout, Options) ->
+connect(ServerName, ServerList, Timeout, Options) ->
     erlzk_conn_sup:start_conn([ServerName, ServerList, Timeout, Options]).
 
-stop(Pid) ->
+close(Pid) ->
     erlzk_conn:stop(Pid).
 
 %% ===================================================================
--- src/erlzk_app.erl
+++ src/erlzk_app.erl
@@ -2,9 +2,25 @@
 -behaviour(application).
 
 -export([start/2, stop/1]).
+-export([ensure_deps_started/0]).
 
 start(_Type, _Args) ->
     erlzk_sup:start_link().
 
 stop(_State) ->
     ok.
+
+ensure_deps_started() ->
+    {ok, Deps} = application:get_key(erlzk, applications),
+    true = lists:all(fun ensure_started/1, Deps).
+
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            true;
+        {error, {already_started, App}} ->
+            true;
+        Else ->
+            error_logger:error_msg("Couldn't start ~p: ~p", [App, Else]),
+            Else
+    end.
