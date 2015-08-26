7ade2c07e8b6053698154574e8bb0ad796e3d443
kevinbombadil <kieran@widgets.net.nz>
2015-08-26 14:53:20 +1200

    Squashed version of commits to enable reconnecting after server fails to heartbeat and provide kill_session function so session timeouts can be tested
    handle heartbeats locally
    
    .. so that a network failure will result in a re-connnect.
    
    typo
    
    more typo
    
    typos
    
    Update erlzk_conn.erl
    
    tweak
    
    Add kill_session function and rework heartbeating
    
    drop pesky log message

--- src/erlzk_heartbeat.erl
+++ src/erlzk_heartbeat.erl
@@ -1,8 +1,12 @@
 %%%-------------------------------------------------------------------
 %%% @author kevinbombadil
 %%% @doc
-%%%   receive heartbeats from the erlzk_conn, send a 'no_heartbest'
-%%%   back to it if we've had to wait too long.
+%%%   Given a callback and an interval start a timer.
+%%%   When you receive a beat, reset the timer.
+%%%   When the timer expires run the callback.
+%%%   Note:
+%%%      - We ignore a noproc exit from the callback
+%%%      - This module terminates on timeout
 %%% @end
 %%%-------------------------------------------------------------------
 -module(erlzk_heartbeat).
@@ -10,64 +14,70 @@
 -behaviour(gen_fsm).
 
 %% API
--export([start_link/2, beat/1, no_beat/1]).
+-export([start_link/2, beat/1]).
 
 %% gen_fsm callbacks
 -export([init/1, beating/2, handle_event/3,
-	 handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
+         handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
 
 -define(SERVER, ?MODULE).
+-define(NOBEAT, no_beat).
 
--record(state, {connection, interval, timer}).
+-record(state, {callback, interval, timer}).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
-start_link(Connection, PartInterval) ->
-    gen_fsm:start_link(?MODULE, [Connection, PartInterval], []).
+start_link(Callback, Interval) ->
+  gen_fsm:start_link(?MODULE, [Callback, Interval], []).
 
 beat(Pid) ->
-    gen_fsm:send_event(Pid, {beat}).
-
-no_beat(Pid) ->
-    gen_fsm:send_event(Pid, {no_beat}).
-
+  gen_fsm:send_event(Pid, {beat}).
 
 %%%===================================================================
 %%% gen_fsm callbacks
 %%%===================================================================
 
-init([Connection, PartInterval]) ->
-    Interval = PartInterval * 3,
-    Timer=gen_fsm:start_timer(Interval,no_beat),
-    {ok, beating, #state{connection=Connection, 
-			 timer=Timer,
-			 interval=Interval}}.
+init([Callback, Interval]) ->
+  Timer=gen_fsm:start_timer(Interval,?NOBEAT),
+  process_flag(trap_exit, true),
+
+  {ok, beating, #state{callback=Callback,
+                       timer=Timer,
+                       interval=Interval}}.
 %%--------------------------------------------------------------------
 
-beating({beat},State=#state{interval=Interval, timer=Timer}) ->
-    gen_fsm:cancel_timer(Timer),
-    Timer1=gen_fsm:start_timer(Interval,no_beat),
-    {next_state, beating, State#{timer=Timer1}};
+beating({beat},State=#state{interval=Interval,timer=Timer}) ->
+%  error_logger:info_msg("Heartbeat: beat~n"),
+  gen_fsm:cancel_timer(Timer),
+  Timer1 = gen_fsm:start_timer(Interval,{?NOBEAT}),
+  {next_state, beating, State#state{timer=Timer1}};
 
-beating({timeout,_Timer,no_beat},State=#state{Connection=connection}) ->
-    erlzk_conn:no_heartbeat(Connection),
-    {stop,shutdown,State}.
+beating({timeout,_Timer,{?NOBEAT}},State=#state{callback=Callback}) ->
+  error_logger:info_msg("Heartbeat: timeout~n"),
+  try
+    {M,F,A}=Callback,
+    M:F(A)
+  catch exit:{noproc,_} ->
+    ok
+  end,
+  {stop,shutdown,State}.
 
 %%--------------------------------------------------------------------
 handle_event(_Event, StateName, State) ->
-    {next_state, StateName, State}.
+  {next_state, StateName, State}.
 
 handle_sync_event(_Event, _From, StateName, State) ->
-    Reply = ok,
-    {reply, Reply, StateName, State}.
+  Reply = ok,
+  {reply, Reply, StateName, State}.
 
 handle_info(_Info, StateName, State) ->
-    {next_state, StateName, State}.
+  {next_state, StateName, State}.
 
 terminate(_Reason, _StateName, _State) ->
-    ok.
+  ok.
 
 code_change(_OldVsn, StateName, State, _Extra) ->
-    {ok, StateName, State}.
+  {ok, StateName, State}.
 
+%%--------------------------------------------------------------------
