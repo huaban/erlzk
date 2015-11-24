88df1756ccde211631b0ef627b4207347ade5e37
belltoy <belltoy@gmail.com>
2015-11-24 16:25:46 +0800

    Add a Hex package definition

---
+++ mix.exs
@@ -0,0 +1,17 @@
+defmodule Erlzk.MixFile do
+  use Mix.Project
+
+  def project do
+    [app: :erlzk,
+     version: "0.6.0",
+     description: "A Pure Erlang ZooKeeper Client (no C dependency)",
+     package: package]
+  end
+
+  def package do
+    [files: ~w(src include rebar.config README.md LICENSE Makefile),
+     licenses: ["Apache 2.0"],
+     maintainers: ["Mega Yu", "Belltoy Zhao"],
+     links: %{"GitHub" => "https://github.com/huaban/erlzk"}]
+  end
+end
